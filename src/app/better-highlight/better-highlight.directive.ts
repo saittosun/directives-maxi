2af364242c2c4db0986d05d28ed8e12a38cf3ea4
Tosun Sait <tosunsa@cronos.be>
2020-02-27 07:04:45 +0100

    binding directive properties

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,14 +1,18 @@
-import { Directive, Renderer2, OnInit, ElementRef, HostListener, HostBinding } from '@angular/core';
+import { Directive, Renderer2, OnInit, ElementRef, HostListener, HostBinding, Input } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
+  @Input() defaultColor: string = 'transparent';
+  // @Input() highlightColor: string = 'blue';
+  @Input('appBetterHighlight') highlightColor: string;
 
   constructor(private renderer: Renderer2,
               private elRef: ElementRef) { }
   
   ngOnInit() {
+    this.backgroundColor = this.defaultColor;
     // this.renderer.setStyle(this.elRef.nativeElement,
     //                        'backgroundColor', 'blue');
   }
@@ -17,15 +21,15 @@ export class BetterHighlightDirective implements OnInit {
   @HostListener('mouseenter') mouseover(eventData: Event) {
     // this.renderer.setStyle(this.elRef.nativeElement,
     //                       'backgroundColor', 'blue');
-    this.backgroundColor = 'blue';
+    this.backgroundColor = this.highlightColor;
   }
 
   @HostListener('mouseleave') mouseleave(eventData: Event) {
     // this.renderer.setStyle(this.elRef.nativeElement,
     //                       'backgroundColor', 'pink');
-    this.backgroundColor = 'transparent';
+    this.backgroundColor = this.defaultColor;
   }
 
   // in @HostBinding, we can pass a string defining to which property of the hosting element we want to bind.
-  @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent';
+  @HostBinding('style.backgroundColor') backgroundColor: string;
 }
